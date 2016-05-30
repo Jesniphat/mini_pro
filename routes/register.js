7638a4904f631ae40164d4697e49220a4e756a8d
jes <jesniphat@hotmail.com>
2016-05-30 08:00:29 +0700

    add del angular

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource (actions of users)');
+  res.render("register.html");
+});
+
+router.post('/save', function(req, res, next) {
+  console.log(req.body);
+  res.send("Save success");
 });
 
 module.exports = router;
